e3ffcdfb24e9dbe53307ed45012d428330650a98
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-21 11:08:08 -0500

    Add models and migrations

---
+++ db/migrate/20170421160123_create_requests.rb
@@ -0,0 +1,13 @@
+class CreateRequests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :requests do |t|
+      t.integer :song_id, null: false
+      t.integer :artist_id, null: false
+      t.integer :genre_id, null: false
+      t.integer :source_id, null: false
+      t.boolean :in_system?
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20170421160137_create_sources.rb
@@ -0,0 +1,10 @@
+class CreateSources < ActiveRecord::Migration[5.0]
+  def change
+    create_table :sources do |t|
+      t.string :name, null: false
+
+
+      t.timestamps null: false
+    end
+  end
+end
