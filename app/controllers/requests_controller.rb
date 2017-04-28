b52e9e22febc94ca40a08bfe178ceb787e5e8eae
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-28 13:53:40 -0500

    Fix routes and start analytics page

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,30 @@
+class RequestsController < ApplicationController
+
+  def new
+  end  
+
+  def create
+    artist = new_artist_or_nah(params[:requests][:artist])
+    song = new_song_or_nah(params[:requests][:song])
+    genre = new_genre_or_nah(params[:requests][:genre])
+    source = new_source_or_nah(params[:requests][:source])
+    log = new_log_or_nah(Time.now.month, Time.now.year)
+
+    request_params = {
+    :song_id => song.id,
+    :artist_id => artist.id,
+    :genre_id => genre.id,
+    :source_id => source.id,
+    :requestlog_id => log.id,
+    :in_system? => params[:requests][:in_library]
+  }
+  request = Request.create(request_params)
+  redirect_to requestlogs_path  
+  end
+
+  private
+  def request_params
+    params.require(:requests).permit(:song_id, :artist_id, :genre_id, :source_id, :requestlog_id, :in_system?)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'home#index'
-  resources :requestlogs, only: [:index, :new, :create, :show]
+  resources :requests, only: [:new, :create]
+  resources :requestlogs, only: [:index, :show]
 end
